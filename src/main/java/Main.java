bc68a3fc530755e2f6d96be28562f712190d0750
Affan Haq <haqgamer66@gmail.com>
2020-11-28 20:45:38 -0500

    added documentation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,20 +4,21 @@ import java.sql.SQLException;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException, SQLException
-    {
+    public static void main(String[] args) {
+
+        // read file from https://opendata.citywindsor.ca/Uploads/Schools.csv and save to schools.csv
         try (BufferedInputStream inputStream = new BufferedInputStream(new URL("https://opendata.citywindsor.ca/Uploads/Schools.csv").openStream());
-             FileOutputStream fileOS = new FileOutputStream("/Schools.csv")) {
-            byte data[] = new byte[1024];
+             FileOutputStream fileOS = new FileOutputStream("src/main/resources/schools.csv")) {
+            byte[] data = new byte[1024];
             int byteContent;
             while ((byteContent = inputStream.read(data, 0, 1024)) != -1) {
                 fileOS.write(data, 0, byteContent);
             }
         } catch (IOException e) {
-            // handles IO exceptions
+            e.printStackTrace();
         }
 
-        new loadData();
-
+        // read schools.csv and insert data into schools table
+        new LoadData("schools.csv");
     }
 }
--- src/main/java/loadData.java
+++ src/main/java/loadData.java
@@ -3,17 +3,12 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
-public class loadData
-{
-    public loadData()
-    {
-        try (InputStream inputStream = Main.class.getResourceAsStream("Schools.csv");
-             BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream)))
-        {
+public class LoadData {
+    public LoadData(String path) {
+        try (InputStream inputStream = Main.class.getResourceAsStream(path);
+             BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
             new Data(reader);
-        }
-        catch (IOException e)
-        {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }

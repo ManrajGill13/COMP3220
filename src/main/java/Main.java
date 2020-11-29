95c95fa7a64ff34a04ede3cb0796d19cc99686f8
Affan Haq <haqgamer66@gmail.com>
2020-11-28 21:48:07 -0500

    scraping can now detect the files that belong to each card

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,14 +11,33 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
+        // connect to the opendata site to scrape it
         Document doc = Jsoup.connect("https://opendata.citywindsor.ca/").get();
 
         System.out.printf("Page: %s\n", doc.title());
 
         // select all the cards and print their titles
-        Elements cards = doc.select("#opendata-container div div div h4");
-        for (Element cardTitle : cards) {
-            System.out.println(cardTitle.text());
+        Elements cards = doc.select("#opendata-container div div div");
+        for (Element card : cards) {
+
+            // print out the card title
+            System.out.print(card.getElementsByClass("card-title").text());
+
+            // get all the files in the footer
+            Elements files = card.select(".card-footer i");
+            for (Element file : files) {
+
+                String title = file.attr("title");
+
+                if (title.toLowerCase().contains("show details")) {
+                    continue;
+                }
+
+                System.out.printf("\t -> %s\n", file.attr("title"));
+
+                // TODO: download each file -> https://opendata.citywindsor.ca/Uploads/${file.attr("title")}
+            }
+            System.out.println();
         }
 
         /*// read file from https://opendata.citywindsor.ca/Uploads/Schools.csv and save to schools.csv

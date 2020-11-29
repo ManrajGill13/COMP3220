4bcf31184a6bb434d592aedaa415a38628a15699
lufem9 <51453231+lufem9@users.noreply.github.com>
2020-11-28 20:30:32 -0500

    Add files via upload
    Update on dynamic source

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,21 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.net.URL;
 import java.sql.SQLException;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException, SQLException {
+    public static void main(String[] args) throws IOException, SQLException
+    {
+        try (BufferedInputStream inputStream = new BufferedInputStream(new URL("https://opendata.citywindsor.ca/Uploads/Schools.csv").openStream());
+             FileOutputStream fileOS = new FileOutputStream("/Schools.csv")) {
+            byte data[] = new byte[1024];
+            int byteContent;
+            while ((byteContent = inputStream.read(data, 0, 1024)) != -1) {
+                fileOS.write(data, 0, byteContent);
+            }
+        } catch (IOException e) {
+            // handles IO exceptions
+        }
 
         new loadData();
 

cc1315fc111ad794a6babcc4532595563810dc87
Affan Haq <haqgamer66@gmail.com>
2020-11-28 21:28:57 -0500

    added the ability to scrape https://opendata.citywindsor.ca/

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,27 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 import java.io.*;
 import java.net.URL;
 import java.sql.SQLException;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        // read file from https://opendata.citywindsor.ca/Uploads/Schools.csv and save to schools.csv
+        Document doc = Jsoup.connect("https://opendata.citywindsor.ca/").get();
+
+        System.out.printf("Page: %s\n", doc.title());
+
+        // select all the cards and print their titles
+        Elements cards = doc.select("#opendata-container div div div h4");
+        for (Element cardTitle : cards) {
+            System.out.println(cardTitle.text());
+        }
+
+        /*// read file from https://opendata.citywindsor.ca/Uploads/Schools.csv and save to schools.csv
         try (BufferedInputStream inputStream = new BufferedInputStream(new URL("https://opendata.citywindsor.ca/Uploads/Schools.csv").openStream());
              FileOutputStream fileOS = new FileOutputStream("src/main/resources/schools.csv")) {
             byte[] data = new byte[1024];
@@ -19,6 +34,6 @@ public class Main {
         }
 
         // read schools.csv and insert data into schools table
-        new LoadData("schools.csv");
+        new LoadData("schools.csv");*/
     }
 }

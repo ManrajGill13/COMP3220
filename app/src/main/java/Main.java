b176b0e3e6adb2bcd4b20f8d182afa3f33f72468
lufem9 <51453231+lufem9@users.noreply.github.com>
2020-11-29 21:06:52 -0500

    Add files via upload
    update on codes

--- app/src/main/java/Main.java
+++ app/src/main/java/Main.java
@@ -1,36 +1,13 @@
-import connection.Connection;
-import connection.ConnectionBuilder;
-import connection.exception.ConnectionBuildException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-
-import java.io.BufferedInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.net.URL;
 
-public class Main {
+public class Main
+{
+    public static void main(String[] args) throws IOException {
 
-    public static Connection DB_CONNECTION;
-
-    public static void main(String[] args) throws ConnectionBuildException {
-
-        // building the connection
-        DB_CONNECTION = new ConnectionBuilder()
-                .setIp("192.168.2.91")
-                .setPort(15896)
-                .setDatabase("windsor")
-                .setUser("root")
-                .setPassword("amBWZSCnZfGLe9U8ZKCMDQEXrxRCCT")
-                .build();
-
-        //scrapeCardsOnWindsorSite();
-        //downloadFileAndLoadIntoDatabase();
-    }
-
-    private static void scrapeCardsOnWindsorSite() throws IOException {
         // connect to the opendata site to scrape it
         Document doc = Jsoup.connect("https://opendata.citywindsor.ca/").get();
 
@@ -59,22 +36,10 @@ public class Main {
             }
             System.out.println();
         }
-    }
 
-    private static void downloadFileAndLoadIntoDatabase() {
-        // read file from https://opendata.citywindsor.ca/Uploads/Schools.csv and save to schools.csv
-        try (BufferedInputStream inputStream = new BufferedInputStream(new URL("https://opendata.citywindsor.ca/Uploads/Schools.csv").openStream());
-             FileOutputStream fileOS = new FileOutputStream("src/main/resources/schools.csv")) {
-            byte[] data = new byte[1024];
-            int byteContent;
-            while ((byteContent = inputStream.read(data, 0, 1024)) != -1) {
-                fileOS.write(data, 0, byteContent);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        // read schools.csv and insert data into schools table
-        new LoadData("schools.csv");
+       // Retrieve data from the data source.
+       loadData.retrieveData("https://opendata.citywindsor.ca/Uploads/Schools.csv","C:\\Users\\BukayoDan\\COMP3220-Elaboration-Phase\\src\\main\\resources\\Schools.csv");
+        // read retrieved data and insert data into schools table
+        new loadData();
     }
 }

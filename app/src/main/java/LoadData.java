b571c44289dbd42df5234085b3bd1531caeb281d
Affan Haq <haqgamer66@gmail.com>
2020-11-29 22:04:45 -0500

    Update LoadData.java

--- app/src/main/java/LoadData.java
+++ app/src/main/java/LoadData.java
@@ -1,14 +1,31 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.net.URL;
 
-public class LoadData {
-    public LoadData(String path) {
-        try (InputStream inputStream = Main.class.getResourceAsStream(path);
-             BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
-            new Data(reader);
+public class loadData
+{
+    public static void retrieveData(String url, String doc){
+        try (BufferedInputStream inputStream = new BufferedInputStream(new URL(url).openStream());
+             FileOutputStream fileOS = new FileOutputStream(doc))
+        {
+            byte[] data = new byte[1024];
+            int byteContent;
+            while ((byteContent = inputStream.read(data, 0, 1024)) != -1)
+            {
+                fileOS.write(data, 0, byteContent);
+            }
         } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public loadData()
+    {
+        try (InputStream inputStream = Main.class.getResourceAsStream("Schools.csv");
+             BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream)))
+        {
+            new Data(reader);
+        }
+        catch (IOException e)
+        {
             e.printStackTrace();
         }
     }
